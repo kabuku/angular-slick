24855e153c5dbd663ec363eeed7bc7871e0f8b77
Atsushi Hanaoka <grktsh@gmail.com>
2017-12-18 12:08:42 +0900

    Wrap in IIFE

--- dist/slick.js
+++ dist/slick.js
@@ -1,3 +1,4 @@
+(function() {
 'use strict';
 angular.module('slick', []).directive('slick', [
   '$timeout',
@@ -160,3 +161,4 @@ angular.module('slick', []).directive('slick', [
     };
   }
 ]);
+})();
